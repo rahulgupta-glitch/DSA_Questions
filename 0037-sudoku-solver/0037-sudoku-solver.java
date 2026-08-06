e90f5fcd7fc2c354d05b29f7a82fd82bbc2989f6
rahulgupta-glitch <rahulrock6394@gmail.com>
2026-08-06 13:25:21 +0530

    Time: 407 ms (5.18%), Space: 43.1 MB (8.65%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char d = '1'; d <= '9'; d++) {
+                        
+                        if (isValid(board, row, col, d)) {
+                            board[row][col] = d;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = '.'; 
+                        }
+                    }
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) {
+                return false;
+            }
+            if (board[i][col] == c) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
