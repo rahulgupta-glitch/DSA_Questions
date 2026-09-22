1ff54f6ed7887b0b38884e074509bcde3a7eb773
rahulgupta-glitch <rahulrock6394@gmail.com>
2026-09-22 22:42:10 +0530

    Time: 5 ms (83.58%), Space: 45.8 MB (86.63%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int y=0;
+        int temp=x;
+        if(x<0) return false;
+        while(x!=0){
+            int digit=x%10;
+            y=y*10+digit;
+            x/=10;
+        }
+        if(y!=temp) return false;
+        else return true;
+    }
+}
